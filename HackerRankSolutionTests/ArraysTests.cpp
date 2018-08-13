11782ae51c5b1854fb268e0a6e123d1e134032da
Eric Brown <Brown.Eric.utmd@gmail.com>
2018-08-12 19:45:41 -0600

    Initial Commit
    Created projects as sandbox/exhibit for HackerRank solutions. Things will be added in an arbitrary fashion as I mess around in HackerRank.

---
+++ HackerRankSolutionTests/ArraysTests.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HackerRankSolutionTests
+{		
+	TEST_CLASS(ArraysTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
